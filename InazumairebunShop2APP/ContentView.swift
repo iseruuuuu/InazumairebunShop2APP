a354147c3ef0755d44723c155f31c6f871c0508b
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2020-11-06 16:57:18 +0900

    Initial Commit

---
+++ InazumairebunShop2APP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InazumairebunShop2APP
+//
+//  Created by user on 2020/11/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InazumairebunShop2APP/InazumairebunShop2APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  InazumairebunShop2APPApp.swift
+//  InazumairebunShop2APP
+//
+//  Created by user on 2020/11/06.
+//
+
+import SwiftUI
+
+@main
+struct InazumairebunShop2APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
